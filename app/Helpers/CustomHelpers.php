cf64fa5b7b078916afed60e0ed8db0799c9dafd9
Sebastian <sebastiancurtislavarias@gmail.com>
2022-10-03 00:39:51 +0800

    Reset gitignore

--- app/Helpers/CustomHelpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists("customResponse")) {
-    function customResponse($data = null, $message = null)
-    {
-        return new \App\Helpers\ExtendedResponse($data, $message);
-    }
-}
